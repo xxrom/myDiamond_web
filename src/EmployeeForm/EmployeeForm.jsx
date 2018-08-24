4e2b65cc6cac812ba402899c84d3b0407f88919f
Chernyshov <xpom55@mail.ru>
2018-08-24 21:42:28 +0300

    make new buttons for adding new block and deleteing last one

--- src/EmployeeForm/EmployeeForm.jsx
+++ src/EmployeeForm/EmployeeForm.jsx
@@ -30,27 +30,30 @@ class EmployeeForm extends Component {
       });
     };
 
-    const structureEmployee = {
+    // Скрытая структура сотрудника
+    this._structureEmployee = {
       name: 'employee',
       schema: schema.employee,
       schemaValues: schema.employee_values,
     };
-    const structureRate = {
+    // Скрытая структура рэйта
+    this._structureRate = {
       name: 'rate',
       schema: schema.rate,
       schemaValues: schema.rate_values,
     };
-    const structure = [
-      structureEmployee,
-      structureRate,
-      structureRate,
-      structureRate,
-      structureRate,
-      structureRate,
+    // Скрытая новая структуру =)
+    this._structure = [
+      this._structureEmployee,
+      this._structureRate,
+      this._structureRate,
+      this._structureRate,
+      this._structureRate,
+      this._structureRate,
     ];
 
-    // генерим новую структуру с учетом расположения эелментов
-    this.structure = structure.map((item, index) => ({
+    // Генерим новую структуру с учетом расположения эелментов
+    this.structure = this._structure.map((item, index) => ({
       ...item,
       name: `${name}${index}`,
     }));
@@ -63,6 +66,53 @@ class EmployeeForm extends Component {
 
     this.state = { values };
   }
+
+  /**
+   * Добавляем новый блок
+   */
+  onAddBlock = () => {
+    console.log('add rate');
+    // Добавляем новый блок к скрытой начальной структуре
+    this._structure.push(this._structureRate);
+
+    // Вычисляем параметры нового элемента
+    const addedIndex = this._structure.length - 1;
+    const addedItem = this._structure[addedIndex];
+    console.log(addedItem);
+    // В структуру закидываем все его данные и генерим имя
+    this.structure.push({
+      ...addedItem,
+      name: `${addedItem.name}${addedIndex}`,
+    });
+
+    // Вытаскиваем данные из только что добавленного элемента
+    const { name, schemaValues } = this.structure[addedIndex];
+    // Добавляем schemaValues для нового блока
+    this.setState({
+      values: {
+        ...this.state.values,
+        [name]: schemaValues,
+      },
+    });
+  };
+
+  /**
+   * Удаления блока
+   */
+  onDelBlock = () => {
+    console.log('del block');
+    this._structure.pop();
+    const deletedBlock = this.structure.pop();
+
+    const values = { ...this.state.values };
+    delete values[deletedBlock.name];
+    console.log(values);
+    this.setState({
+      values: {
+        ...values,
+      },
+    });
+  };
 
   render() {
     console.log(this.structure);
@@ -81,6 +131,8 @@ class EmployeeForm extends Component {
       <div>
         <h3 className="title">Добавить нового сотрудника</h3>
         {Diamond}
+        <button onClick={this.onAddBlock}>Добавить rate</button>
+        <button onClick={this.onDelBlock}>Удалить rate</button>
         <SubmitButton
           onSubmit={this.onSubmit}
           disabled={true}
